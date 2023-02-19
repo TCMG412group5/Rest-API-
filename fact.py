c945db71adddfdf00d9614d7812b06c5e8ff0c81
leonelsortols <123771044+leonelsortols@users.noreply.github.com>
2023-02-19 15:04:41 -0600

    added fibon

--- fact.py
+++ fact.py
@@ -8,5 +8,17 @@ def factorial(num):
         
     return jsonify({'input' : num ,'output' : factorial })
 
+@app.route("/Fib/<int:num>",methods=['GET'])
+def fib(num):
+    l = [0,1] 
+    if num < len(l):
+        return jsonify ({"Input": num, "output" : l})
+    else:
+        for i in range (len(l), num+1):
+                sum=l[i-1] +l[i-2]
+                l.append(sum)
+            
+    return jsonify({"input" : num, "output" : l})
 if __name__ == '__main__':
     app.run(port = 4000,debug=True)
+
