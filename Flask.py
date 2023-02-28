db7ec9316a1e912880aed1bc16ffc8d9d44d9247
Mattic <50807277+ImMattic@users.noreply.github.com>
2023-02-27 21:51:45 -0600

    fixed issues causing failed unit tests (#10)
    Co-authored-by: Matt <jacobie4@gmail.com>

--- Flask.py
+++ Flask.py
@@ -13,10 +13,7 @@ def hello_world():
 
 
 # Route for the /md5 endpoint
-# The 2 other routes are to account for no parameters passed
-@app.route("/md5", methods=["GET"])
-@app.route("/md5/", methods=["GET"])
-@app.route("/md5/<path:data>", methods=["GET"])
+@app.route("/md5/<string:data>", methods=["GET"])
 def get_md5(data=""):
     # If an endpoint is provided with no data, return a value of None
     if data == "":
@@ -25,14 +22,12 @@ def get_md5(data=""):
     # Otherwise, use the MD5 algorithm on the provided data
     else:
         md5_hash = md5(data.encode()).hexdigest()
-    return jsonify({"md5_hash": md5_hash}), 200
+    return jsonify({"input": data, "output": md5_hash}), 200
 
 
 # Route for the /is-prime endpoint
-@app.route("/is-prime", methods=["GET"])
-@app.route("/is-prime/", methods=["GET"])
-@app.route("/is-prime/<path:data>", methods=["GET"])
-def get_isprime(data = ""):
+@app.route("/is-prime/<int:data>", methods=["GET"])
+def get_isprime(data=""):
     primecheck = True
     if data == "":
         primenumber = None
@@ -40,7 +35,7 @@ def get_isprime(data = ""):
     else:
         primenumber = int(data)
         if primenumber > 1:
-            for i in range(2, int(primenumber**0.5)+1 ):
+            for i in range(2, int(primenumber**0.5)+1):
                 if primenumber % i == 0:
                     primecheck = False
                     break
@@ -48,18 +43,15 @@ def get_isprime(data = ""):
                     primecheck = True
         else:
             primecheck = False
-    return jsonify({"input:": primenumber, "output:" : primecheck}), 200
+    return jsonify({"input": primenumber, "output": primecheck}), 200
 
 
 # Route for the /slack-alert endpoint
-# The 2 other routes are to account for no parameters passed
-@app.route("/slack-alert", methods=["GET"])
-@app.route("/slack-alert/", methods=["GET"])
 @app.route("/slack-alert/<string:data>", methods=["GET"])
 def slack_alert(data=""):
     # Returns a 400 error if there's no input
     if data == "":
-        return jsonify({"message_sent": False}), 400
+        return jsonify({"input": data, "output": False}), 400
 
     else:
         # Uses the webhook url from web_hook.txt
@@ -70,34 +62,37 @@ def slack_alert(data=""):
         response = requests.post(slack_webhook, data=message)
 
         # If a 200 code with an ok message is returned, return a JSON with
-        # {"message_sent": True}
+        # {"output": True}
         if response.status_code == 200 and response.text == "ok":
-            return jsonify({"message_sent": True}), 200
+            return jsonify({"input": data, "output": True}), 200
 
         # If anything else is returned, return a JSON with
-        # {"message_sent": False}
+        # {"output": False}
         else:
-            return jsonify({"message_sent": False}), 400
-            
-#Route for factorial 
-@app.route("/factorial/<int:num>",methods=['GET'])
-def factorial(num):
-    factorial = 1 
-    for i in range(1,num +1):
-        factorial = factorial*i
-        
-    return jsonify({'input' : num ,'output' : factorial })
+            return jsonify({"input": data, "output": False}), 400
 
-#Route for fib seqeunce 
-@app.route("/fibonacci/<int:num>",methods=['GET'])
+
+# Route for factorial
+@app.route("/factorial/<int:num>", methods=['GET'])
+def factorial(num):
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial = factorial*i
+
+    return jsonify({'input': num, 'output': factorial})
+
+
+# Route for fib seqeunce
+@app.route("/fibonacci/<int:num>", methods=['GET'])
 def fib(num):
-    l = [] 
+    l = []
     a = 0
-    b =1
+    b = 1
     while a <= num:
         l.append(a)
-        a,b= b,a+b            
-    return jsonify({"input" : num, "output" : l})
+        a, b = b, a+b
+    return jsonify({"input": num, "output": l})
+
 
 # Runs the Flask application
 if __name__ == '__main__':
