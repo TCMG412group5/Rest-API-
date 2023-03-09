1e41b4f44257da6f884ef860847673714b537b4d
Matt <jacobie4@gmail.com>
2023-03-09 11:32:25 -0600

    code cleanup and fixed test_endpoint bug

--- tests.py
+++ tests.py
@@ -15,7 +15,7 @@ def test_endpoint(input, expected_response, expected_code):
             print(str(input) + " - \u2705 SUCCESS")
             sys.exit(0)
     elif (response.status_code == 200 and response.text.strip() ==
-          expected_response):
+          expected_response and response.status_code == expected_code):
         print(str(input) + " - \u2705 SUCCESS")
         sys.exit(0)
     else:
@@ -33,72 +33,91 @@ def test_endpoint(input, expected_response, expected_code):
 # 2) Expected text output
 # 3) Expected code
 test_list = [
-    test_list = [
+    # MD5 Test Case 1
     ("/md5/",
      "",
      404),
+    # MD5 Test Case 2
     ("/md5/hello_world",
      '{"input":"hello_world","output":"99b1ff8f11781541f7f89f9bd41c4a17"}',
      200),
-     ("/md5/test!",
-      '{"input":"test!","output":"c4d354440cb41ee38e162bc1f431e99b"}',
-      200),
-      ("/md5/5um",
-      '{"input":"5um","output":"13db2c82a7b5cbdd32a5b7ead600f503"}',
-      200),
-      ("/is-prime/5",
-       '{"input":5,"output":true}',
-       200),
-       ("/is-prime/6",
-       '{"input":6,"output":false}',
-       200),
-       ("/is-prime/six",
-       '',
-       404),
-       ("/is-prime/7",
-       '{"input":7,"output":true}',
-       200),
+    # MD5 Test Case 3
+    ("/md5/test!",
+     '{"input":"test!","output":"c4d354440cb41ee38e162bc1f431e99b"}',
+     200),
+    # MD5 Test Case 4
+    ("/md5/5um",
+     '{"input":"5um","output":"13db2c82a7b5cbdd32a5b7ead600f503"}',
+     200),
+    # is-prime Test Case 1
+    ("/is-prime/5",
+     '{"input":5,"output":true}',
+     200),
+    # is-prime Test Case 2
+    ("/is-prime/6",
+     '{"input":6,"output":false}',
+     200),
+    # is-prime Test Case 3
+    ("/is-prime/six",
+     '',
+     404),
+    # is-prime Test Case 4
+    ("/is-prime/7",
+     '{"input":7,"output":true}',
+     200),
+    # factorial Test Case 1
     ("/factorial/5",
-      '{"input":5,"output":120}',
-      200),
-      ("/factorial/$6",
-       "",
-       404),
-       ("/factorial/3",
-        '{"input":3,"output":6}',
-        200),
-        ("/factorial/7",
-        '{"input":7,"output":5040}',
-        200),
-       ("/fibonacci/8",
-        '{"input":8,"output":[0,1,1,2,3,5,8]}',
-        200),
-        ("/fibonacci/eight",
-         "",
-         404),
-         ("/fibonacci/37",
-          '{"input":37,"output":[0,1,1,2,3,5,8,13,21,34]}',
-          200),
-          ("/fibonacci/41",
-          '{"input":41,"output":[0,1,1,2,3,5,8,13,21,34]}',
-          200),
-          
-]
+     '{"input":5,"output":120}',
+     200),
+    # factorial Test Case 2
+    ("/factorial/$6",
+     "",
+     404),
+    # factorial Test Case 3
+    ("/factorial/3",
+     '{"input":3,"output":6}',
+     200),
+    # factorial Test Case 4
+    ("/factorial/7",
+     '{"input":7,"output":5040}',
+     200),
+    # fibonacci Test Case 1
+    ("/fibonacci/8",
+     '{"input":8,"output":[0,1,1,2,3,5,8]}',
+     200),
+    # fibonacci Test Case 2
+    ("/fibonacci/eight",
+     "",
+     404),
+    # fibonacci Test Case 3
+    ("/fibonacci/37",
+     '{"input":37,"output":[0,1,1,2,3,5,8,13,21,34]}',
+     200),
+    # fibonacci Test Case 4
+    ("/fibonacci/41",
+     '{"input":41,"output":[0,1,1,2,3,5,8,13,21,34]}',
+     200),
+    # slack-alert Test Case 1
 ]
 
-tests_failed = False
+
+test_fails = 0
 for test in test_list:
     try:
         test_endpoint(*test)
     except SystemExit as e:
-        if e != 0:
-            test_failure = True
+        if e.code != 0:
+            test_fails += 1
 
-if tests_failed == True:
+if test_fails > 0:
     print()
-    print("A test case failed - Exit Code 1")
+    print("%s/%s test cases passed" %
+          (str(len(test_list)-test_fails), str(len(test_list))))
+    print("Exit code: 1")
     sys.exit(1)
 else:
     print()
-    print("All test cases passed - Exit Code 0")
+    print("%s/%s test cases passed" %
+          (str(len(test_list)), str(len(test_list))))
+    print("Exit code: 0")
     sys.exit(0)
